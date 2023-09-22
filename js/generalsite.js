13883e06ed3c1a6078f22cfc09ae16420b03e231
Kushal Kanodia <kkanodia7@gmail.com>
2023-09-22 17:44:40 -0400

    Initial commit

---
+++ js/generalsite.js
@@ -0,0 +1,9 @@
+function extend(){
+    var x = document.getElementById("userLinks");
+    if (x.style.display === "block"){
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
