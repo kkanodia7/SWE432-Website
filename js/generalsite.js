b864f5c4a3c6fffa3f4ce8673f4550a23378eeb4
Sepehr Naimi <sepehr.naimi.m@gmail.com>
2023-10-03 23:03:28 -0400

    update the css for the search

--- js/generalsite.js
+++ js/generalsite.js
@@ -7,3 +7,19 @@ function extend(){
         x.style.display = "block";
     }
 }
+
+function performSearch() {
+    var query = document.getElementById('search-bar').value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'path_to_server/search.php?q=' + encodeURIComponent(query), true);
+
+    xhr.onload = function() {
+        if (this.status == 200) {
+            // Update the site body with search results
+            document.querySelector('.sitebody').innerHTML = this.responseText;
+        }
+    }
+
+    xhr.send();
+}
