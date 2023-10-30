1e791132f9a954be4684649f49dda040451ed2f5
Kushal Kanodia <kkanodia7@gmail.com>
2023-10-30 16:06:55 -0400

    Added data passing to Listener server

--- Listener/server.js
+++ Listener/server.js
@@ -4,19 +4,22 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/../'));
 
+// Data to pass in
+var name = "Kushal"
+
 // Playlists page
 app.get('/index', function(req, res) {
-    res.render('pages/index.ejs');
+    res.render('pages/index.ejs', {username: name});
 });
 
 // Favorites page
 app.get('/favorites', function(req, res){
-    res.render('pages/favorites.ejs');
+    res.render('pages/favorites.ejs', {username: name});
 })
 
 // Recommended page
 app.get('/recommended', function(req, res) {
-    res.render('pages/recommended.ejs');
+    res.render('pages/recommended.ejs', {username: name});
 });
 
 app.listen(8080);
